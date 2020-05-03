a8e8ad7b48390c1a20982e30b83125a43b2bd648
Hugo Lageneste <hugo@olivia-ai.org>
2020-05-03 21:23:33 +0200

    Begin to write the new lexer with a list of item types

---
+++ lexer/lexer.go
@@ -0,0 +1,32 @@
+package lexer
+
+import "fmt"
+
+// ItemType is the type of Item, types are initialized below in const
+type ItemType int
+
+// An Item is used for the lexer
+type Item struct {
+	Type  ItemType
+	Value string
+}
+
+const (
+	itemError ItemType = iota
+	itemFunction
+	itemIncludes
+	itemString
+	itemInt
+	itemLeftParentheses
+	itemRightParentheses
+	itemPipe
+)
+
+// String methods is the one used by Printf
+func (item Item) String() string {
+	if item.Type == itemError {
+		return item.Value
+	}
+
+	return fmt.Sprintf("%q", item.Value)
+}
