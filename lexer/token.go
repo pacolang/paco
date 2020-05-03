f272d6ad7824b3873a3800a3a784e8cec2104732
Hugo Lageneste <hugo@olivia-ai.org>
2020-05-03 16:15:54 +0200

    Add a few keywords and delimiers and the token structure

---
+++ lexer/token.go
@@ -0,0 +1,22 @@
+package lexer
+
+const (
+	// Special characters
+	LEFT_PARENTHESES  = "("
+	RIGHT_PARENTHESES = ")"
+	COMMENT           = "-"
+	QUOTE             = "'"
+	COMMA             = ","
+
+	// Keywords
+	FUNCTION = "fn"
+	END      = "end"
+	STRING   = "string"
+	INCLUDES = "includes"
+)
+
+// A Token is used to reference a part of the code with its value
+type Token struct {
+	Type  string
+	Value string
+}
