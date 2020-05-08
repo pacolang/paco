5f32b69de2fc9b2ab1b666bd561fac5b83b83666
Hugo Lageneste <hugo@olivia-ai.org>
2020-05-08 20:25:03 +0200

    Add body nodes to conditions

--- parser/conditions.go
+++ parser/conditions.go
@@ -1,18 +1,36 @@
 package parser
 
-// parseCondition
-func parseCondition(parser *Parser) Node {
-	node := Node{
+import (
+	"github.com/hugolgst/paco/lexer"
+	"github.com/hugolgst/paco/log"
+)
+
+// parseCondition parses a condition and its body and returns the node
+func parseCondition(parser *Parser) (node Node) {
+	node = Node{
 		Type: Condition,
+		Params: []Node{
+			parseItem(parser, parser.next()),
+		},
+		Value: parser.next().Value,
 	}
 
+	// Append the last element on the params
+	node.Params = append(
+		node.Params,
+		parseItem(parser, parser.next()),
+	)
+
+	// Add body nodes
 	item := parser.next()
-	node.Params = append(node.Params, parseItem(parser, item))
+	for item.Type != lexer.ItemEnd {
+		node.Body = append(node.Body, parseItem(parser, item))
+		item = parser.next()
 
-	node.Value = parser.next().Value
+		if item.Type == lexer.ItemEOF {
+			log.Errorf("end was not found")
+		}
+	}
 
-	item = parser.next()
-	node.Params = append(node.Params, parseItem(parser, item))
-
-	return node
+	return
 }
