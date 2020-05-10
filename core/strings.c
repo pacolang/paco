c89ff5779c9cd8b95fd3ca3c41c292c2bf0002d2
Hugo Lageneste <hugo@olivia-ai.org>
2020-05-10 14:19:56 +0200

    Create startsWith method in strings.c

--- core/strings.c
+++ core/strings.c
@@ -5,3 +5,10 @@
 int contains(const char *str, const char *substr) {
   return strstr(str, substr) != NULL;
 }
+
+// startsWith checks if a str starts with a substr
+int startsWith(const char *str, const char *substr) {
+  size_t lenpre = strlen(substr),
+           lenstr = strlen(str);
+    return lenstr < lenpre ? 0 : memcmp(substr, str, lenpre) == 0;
+}
--- core/strings.h
+++ core/strings.h
@@ -2,5 +2,6 @@
 #define PACO_STRINGS_H
 
 int contains(const char *str, const char *substr);
+int startsWith(const char *str, const char *substr);
 
 #endif //PACO_STRINGS_H
