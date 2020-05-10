e5ac5511e624886a542c8fdb45118e1e448e0f70
Hugo Lageneste <hugo@olivia-ai.org>
2020-05-10 13:40:57 +0200

    Comment console.c file

--- core/console.c
+++ core/console.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include "console.h"
 
+// println prints the given message with a line break after
 void println(const char *message) {
   puts(message);
 }
 
+// print prints the given message without a line break after
 void print(const char *message) {
   printf("%s", message);
 }
 
+// getStringEntry returns the entered string
 char* getStringEntry() {
   char* entry = (char*) malloc(100);
   scanf("%s", entry);
@@ -17,6 +20,7 @@ char* getStringEntry() {
   return entry;
 }
 
+// getIntEntry returns the entered integer
 int getIntEntry() {
   int entry = 0;
   scanf("%d", &entry);
